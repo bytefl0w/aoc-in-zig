c36d0db686ef209249df973836a0224788183ce0
ByteFl0w <hello@erikmortimer.dev>
2023-06-19 07:21:14 -0400

    feat: Day2 - a better way of reading in files

--- day2/main.zig
+++ day2/main.zig
@@ -2,13 +2,29 @@ const std = @import("std");
 const print = std.debug.print;
 const ArrayList = std.ArrayList;
 
-const data = @embedFile("data.txtstd");
-
 pub fn main() !void {
     std.debug.print("AoC Day2, woohoo!", .{});
-    
+
     const arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
-}
 
+    // A better way of reading in files in Zig I think
+    // Read in file
+    var args = std.process.args();
+
+    // Skipping program name
+    _ = args.next();
+
+    var file_name: []const u8 = "";
+    if (args.next()) |f| {
+        file_name = f;
+    }
+
+    const file = try std.fs.cwd().openFile(file_name, .{});
+    defer file.close();
+
+    const fize_size = try file.getEndPos();
+    var txt_file = try allocator.alloc(u8, fize_size);
+    _ = try file.read(txt_file);
+}
